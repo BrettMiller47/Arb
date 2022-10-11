a7cf4e981f4e3e2f7e3d9c3704a67667b4e211bd
Brett Miller <59093846+BrettMiller47@users.noreply.github.com>
2022-10-11 04:42:24 -0400

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -3,38 +3,35 @@ from collections import OrderedDict
 
 # Iterate through the WebScrape dictionary to replace American values with decimal values
 def arbitrage_result(dict_american_odds):
-    """ Starts with a dictionary (dict_american_odds) where Key: value pairs are {'Team': American Odds}.
+    """ Starts with a dictionary of American sports betting odds where Key: value pairs are {'Team': American Odds}.
 
         Iterates through (dict_american_odds) creating a new dictionary (dict_sorted) where Key: value pairs are
         {'Team': Wager to Win $100}.
 
         If the (dict_american_odds) is not an arbitrage candidate, then (dict_sorted) = {} is returned.
 
-        P.S. - The exit of a non-profitable event must occur in the loop itself to reduce processing effort.
-    :param dict_american_odds:
-    :returns dict_sorted:
+    :param dict_american_odds: {'Team': American Odds}
+    :returns dict_sorted:  {'Team': Wager to Win $100}
     """
-    # Create a dictionary from the passed in dictionary.
-    # Sort this new dictionary so the event's wagers are calculated in order of largest->smallest wager
+    # Create and sort the argument dictionary such that the event's wagers will be largest --> smallest
     dict_sorted = dict(OrderedDict(sorted(dict_american_odds.items(), key=lambda x: x[1])))
     wagers = []
 
-    # Change from {Team: American odds} to {Team: Revenue Rate-of-Change per $0.01 added}
-    # Essentially, for each Key: Value pair in dict_sorted we are calculating the wager that payout $100
+    # For each dict_sorted odds, calculate the wager required for a $100 winning payout (including original stake)
     for value in dict_sorted:
 
         # Iterate through each key: value pair and update with a wager as the new value
         if sum(wagers) >= 100:
             # Exit the loop if the sum of all calculated wagers is >100, the scenario is not profitable
-            print("Exiting the iteration")
+            print("Darn!  No arbitrage opportunity.")
             dict_sorted = {}
             return dict_sorted
 
         else:
-            # Store the absolute value of the odds in a variable to use for calculations of payout
+            # Get the absolute value of the odds
             abs_odds_value = abs(dict_sorted[value][0])
 
-            # Continue the loop until all key: value pairs have wagers calculated
+            # Continue the loop until all odds offers have a calculated wager for $100 payout (including original stake)
             if dict_sorted[value][0] < 0:
                 # Revenue rate of change (negative odds) = 1 / ABS(odds)
                 # Wager for negative odds to payout $100 = 1 / ($0.01 + Revenue rate-of-change)
