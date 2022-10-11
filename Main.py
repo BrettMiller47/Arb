e960b51b7a8dfa8234c52e411b2b805f7f2f3959
Brett Miller <brett.miller47@yahoo.com>
2022-10-11 05:01:04 -0400

    cleaned up final print and comments

--- Main.py
+++ Main.py
@@ -5,8 +5,8 @@ from Functions import arbitrage_result
 import pandas
 
 
-# A dictionary to hold the teams, odds, and 
-og_dict = {
+# A dictionary to hold the teams, odds, and
+starting_dict = {
     'Team1': [350, 'wager'],
     'Team2': [-120, 'wager'],
     'Team3': [1000, 'wager'],
@@ -15,29 +15,30 @@ og_dict = {
 
 
 if __name__ == "__main__":
-    
+
     # Declared a dictionary with wager values added
-    dict_wagers = arbitrage_result(og_dict)
+    dict_wagers = arbitrage_result(starting_dict)
 
     if len(dict_wagers) <= 1:
         print("no arbitrage opportunity")
 
     else:
-        # Create a dataframe for a summary print of the wagering event
+        # Create a data frame for printing purposes
         my_df = pandas.DataFrame(dict_wagers).T
-        print(my_df)
 
-        # Rename the columns appropriately
+        # Rename the columns
         renamed_df = my_df.rename(columns={0: "Odds", 1: "Wagers"})
 
-        # Round the columns appropriately
+        # Round the columns
         renamed_df['Odds'] = renamed_df['Odds'].astype(int)
-        renamed_df['Wagers'] = renamed_df['Wagers'].apply(lambda x: "${:.2f}".format(x))
+        renamed_df['Wagers'] = renamed_df['Wagers'].apply(
+            lambda x: "${:.2f}".format(x))
 
-        # Create variables for formatting final print
-        total_wagers_print = renamed_df['Wagers'].str[1:].astype(float).sum()
+        # Sum the wagers
+        sum_wagers = renamed_df['Wagers'].str[1:].astype(float).sum()
 
-        # Final Print
+        # Final Print -- data frame, Total Wagers, Min. Payout constraint used to calc. wagers
         print(renamed_df)
-        print("Total Wagers = $", total_wagers_print)
+        print("-------------------")
+        print("Total Wagers = $", sum_wagers)
         print("Min. Payout if any win = $100")
