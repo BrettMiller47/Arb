23c159131f3c9b5028ecb8faeacfbc7dc0d96f29
Brett Miller <59093846+BrettMiller47@users.noreply.github.com>
2022-10-11 04:27:28 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,43 +3,21 @@ import numpy as np
 import pandas as pd
 from Functions import arbitrage_result
 import pandas
-from WebScrape import sportsbooks_scrape
 
 
-# dict_web_scrape is the output of WebScrape.py
-# TEMPORARILY THIS IS THE SETUP (WAIT FOR WEB SCRAPE TO TRULY SET UP FUNCTIONS.PY)
-# WE WANT THE SCRIPT TO WORK TO PRINT A DF SUMMARY, THEN REMOVE "Event" and replace with "Team"
-# We can limit dict_web_scrape to identify only 1 event, because wagers will be placed manually (can rerun after)
-# Assume arbitrage opportunities are rare and there's no need to weigh the opportunity cost of one vs another
-predifined_urls = {
-    'Draftkings': 'https://sportsbook.draftkings.com/featured?category=game-lines',
-    'Fanduel': 0,
-    'Fox': 0,
-    'BetMGM': 1,
-    'Ceasars': 1,
-    'Barstool': 0,
-    'TwinSpires': 0,
-    'Parx': 0,
-    'UniBet': 0,
-    'BetRivers': 0
-}
-
-dict_web_scrape = {
+# A dictionary to hold the teams, odds, and 
+og_dict = {
     'Team1': [350, 'wager'],
     'Team2': [-120, 'wager'],
     'Team3': [1000, 'wager'],
     'Team4': [750, 'wager']
 }
 
-print(dict_web_scrape)
 
-
-# dict_web_scrape feeds the arbitrage_result() function and returns either
-# - blank dictionary (no arbitrage opportunity)
-# or
-# - dictionary structured asa {'Team1': [Odds, 'Wager']}
 if __name__ == "__main__":
-    dict_wagers = arbitrage_result(dict_web_scrape)
+    
+    # Declared a dictionary with wager values added
+    dict_wagers = arbitrage_result(og_dict)
 
     if len(dict_wagers) <= 1:
         print("no arbitrage opportunity")
